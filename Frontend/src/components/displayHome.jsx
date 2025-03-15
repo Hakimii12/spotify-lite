a4d88e1baa13bc6723942a71e0fbd21a4fce775c
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2025-03-15 12:49:36 +0300

    #######

--- Frontend/src/components/displayHome.jsx
+++ Frontend/src/components/displayHome.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './navbar'
 import  {albumsData} from '../assets/frontend-assets/assets'
 import AlbumItems from './AlbumItems'
+import { songsData } from '../assets/frontend-assets/assets'
+import SongItems from './songItems'
 function displayHome() {
   return (
   <><Navbar/>
@@ -9,10 +11,17 @@ function displayHome() {
     <h1>Featured charts</h1>
     <div className="flex overflow-auto">
       {albumsData.map((items,index)=>(
-    <AlbumItems key={index} image={items.image} name={items.name} desc={items.desc}/>
+    <AlbumItems id={index} image={items.image} name={items.name} desc={items.desc}/>
    ))}
     </div>
-   
+  </div>
+  <div className="mb-4">
+    <h1>Todays biggest hits</h1>
+    <div className="flex overflow-auto">
+      {songsData.map((items,index)=>(
+    <SongItems id={index} image={items.image} name={items.name} desc={items.desc}/>
+   ))}
+    </div>
   </div>
   </>
   )
