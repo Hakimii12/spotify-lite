5de02940986ee982d453c1a7eda73ebc25e3d499
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2025-03-16 11:22:06 +0300

    #####

--- Frontend/src/components/display.jsx
+++ Frontend/src/components/display.jsx
@@ -1,8 +1,11 @@
+import { useLocation } from 'react-router-dom'
 import React from 'react'
 import {Route,Routes} from 'react-router-dom'
 import DisplayHome from './displayHome'
 import DisplayAlbum from './DisplayAlbum'
 function display() {
+  const location=useLocation()
+  console.log(location.pathname)
   return (
    <div className="w-[100%] m-2 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0 ">
     <Routes>
